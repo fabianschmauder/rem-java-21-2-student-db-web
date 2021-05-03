9480ab84448fb00590fa9acdea54e6a7fdf93cb1
Fabian Schmauder <fabian.schmauder@gmail.com>
2021-05-03 15:47:46 +0200

    Init request param

--- src/main/java/de/neuefische/studentdbweb/controller/StudentController.java
+++ src/main/java/de/neuefische/studentdbweb/controller/StudentController.java
@@ -2,10 +2,7 @@ package de.neuefische.studentdbweb.controller;
 
 import de.neuefische.studentdbweb.model.Student;
 import de.neuefische.studentdbweb.service.StudentService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -17,7 +14,7 @@ public class StudentController {
     private final StudentService studentService = new StudentService();
 
     @GetMapping
-    public List<Student> listStudents() {
+    public List<Student> listStudents(@RequestParam Optional<String> search) {
         return studentService.list();
     }
 
