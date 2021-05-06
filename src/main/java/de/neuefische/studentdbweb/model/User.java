9b64257f6056f1d131522e8d6c1c18f85e15f054
Fabian Schmauder <fabian.schmauder@gmail.com>
2021-05-06 12:39:24 +0200

    Add lombok

--- src/main/java/de/neuefische/studentdbweb/model/User.java
+++ src/main/java/de/neuefische/studentdbweb/model/User.java
@@ -1,56 +1,15 @@
 package de.neuefische.studentdbweb.model;
 
 
-import java.util.Objects;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
     private String name;
     private String id;
     private String username;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        User user = (User) o;
-        return Objects.equals(name, user.name) && Objects.equals(id, user.id) && Objects.equals(username, user.username);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, id, username);
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "name='" + name + '\'' +
-                ", id='" + id + '\'' +
-                ", username='" + username + '\'' +
-                '}';
-    }
 }
